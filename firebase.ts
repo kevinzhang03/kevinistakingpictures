d98069c012cff7e05daf5852eebfc9e0936880df
kevinzhangTT <kevin9019888@gmail.com>
2023-04-29 22:14:12 -0600

    install firebase, add firebase init code

---
+++ firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAt88Mwy53iMzhSu6WQtxsfE0pUtN0v6hM',
+  authDomain: 'kevinistakingpictures-ca.firebaseapp.com',
+  projectId: 'kevinistakingpictures-ca',
+  storageBucket: 'kevinistakingpictures-ca.appspot.com',
+  messagingSenderId: '345899431856',
+  appId: '1:345899431856:web:905e4b61ec600f80b5c875',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
