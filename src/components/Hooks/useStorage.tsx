78e841dcf5ce33dffb6a818fdc1bd0acb268b68f
kevinzhangTT <kevin9019888@gmail.com>
2023-05-02 00:44:46 -0600

    complete useStorage hook

--- src/components/Hooks/useStorage.tsx
+++ src/components/Hooks/useStorage.tsx
@@ -1,8 +1,30 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 
-export const useStorage = () => {
+import { ref, StorageError, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
+import { storage } from '../../config/firebase';
+
+export const useStorage = (file: File) => {
   const [progress, setProgress] = useState(0);
-  const [error, setError] = useState<string | null>(null);
+  const [error, setError] = useState<StorageError | null>(null);
   const [url, setUrl] = useState<string | null>(null);
+
+  useEffect(() => {
+    const storageRef = ref(storage, file.name);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+  
+    uploadTask.on('state_changed', (snapshot) => {
+      setProgress((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
+    }, (err) => {
+      setError(err);
+    }, async () => {
+      await getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+        setUrl(downloadURL);
+        console.log('File available at', downloadURL);
+      });
+    });
+  }, [file]);
+
+  return { progress, url, error };
+  
 };
